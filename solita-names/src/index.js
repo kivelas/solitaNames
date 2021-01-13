f6cf2f1031743045976a39b4d952a45a22a71c42
Saila Kivelä <kivela.saila@gmail.com>
2021-01-13 16:45:17 +0200

    implement printing names in table

--- solita-names/src/index.js
+++ solita-names/src/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import App from './App';
+//import reportWebVitals from './reportWebVitals';
+import Names from './names.json';
 
+class Navigation extends React.Component {
+  render(){
+    return (
+      <div>
+        <button
+          className="list"
+          /*onClick={this.props.onClick}*/>
+            List
+          </button>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render(){
+    return (
+      <div>
+        <table border='2'>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Amount</th>
+            </tr>
+            {Names.names.map((item, i) => (
+              <tr key={i}>
+                <td>{item.name}</td>
+                <td>{item.amount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+//------------
+//------------
+
+ReactDOM.render(
+  <Content />,
+  document.getElementById('root')
+);
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +61,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
