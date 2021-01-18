0d596f1ab24d04bd4910df2c43c6df0b7d73672e
Saila Kivelä <kivela.saila@gmail.com>
2021-01-18 17:24:15 +0200

    implement working buttons

--- solita-names/src/index.js
+++ solita-names/src/index.js
@@ -2,31 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-//import reportWebVitals from './reportWebVitals';
 import Names from './names.json';
 
-/*class Navigation extends React.Component {
-  listNames(){
-    return (
-      <div className="results">
-          <Results/>
-      </div>
-    )
-  }
- 
-  render(){
-    return (
-      <div>
-        <button
-          className="list"
-          onClick={listNames}>
-            List
-          </button>
-      </div>
-    );
-  }
-}
-*/
+
 function Navigation(props) {
   return (
     <button
@@ -36,6 +14,7 @@ function Navigation(props) {
       </button>
   );
 }
+
 
 class NavBar extends React.Component {
   
@@ -64,10 +43,13 @@ class NavBar extends React.Component {
 
 class Results extends React.Component {
   
-  listNames() {
-  
-    const orderedNames= Names.names.sort((b,a) => a.amount - b.amount);
-    return (
+  render() {
+
+    const index = this.props.indexOfClickedButton;
+
+    if(index === 0){
+      const orderedNames= Names.names.sort((b,a) => a.amount - b.amount);
+      return (
         <div>
           <table border='2'>
             <tbody>
@@ -84,13 +66,10 @@ class Results extends React.Component {
             </tbody>
           </table>
         </div>
-      
-    );
-  }
-
-  abcNames() {
-    const abcNames = Names.names.sort((a,b) => a.name.localeCompare(b.name));
-    return (
+      );
+    } else if(index === 1){
+      const abcNames = Names.names.sort((a,b) => a.name.localeCompare(b.name));
+      return (
         <div>
           <table border='2'>
             <tbody>
@@ -105,55 +84,24 @@ class Results extends React.Component {
             </tbody>
           </table>
         </div>
-      
-    );
-  }
+      );
+    } else if(index === 2){
+      let sum = 0;
+      const names = Names.names.sort((b,a) => a.amount - b.amount);
+      sum = names.map(total => total.amount).reduce((acc,total) => total + acc);
+      return (
+        <div>
+          <p>The total sum of the 10 most popular male and female names is <b>{sum}</b>.</p>
+        </div>
+      );  
+    } else if(index === 3){
+      return(
+        <p>3 painettu</p>
+      )
+    }
 
-  sum(){
-    let sum = 0;
-    const names = Names.names.sort((b,a) => a.amount - b.amount);
-    sum = names.map(total => total.amount).reduce((acc,total) => total + acc);
-    return (
-      <div>
-        <p>The total sum of the 10 most popular male and female names is <b>{sum}</b>.</p>
-      </div>
-    );
-  }
-
-  render(){
-    return (
-      <div className="results">
-        {this.listNames()}
-      </div>
-    )
-    /*
-    const clickedItem = this.props.indexOfClickedButton;
-    return(clickedItem);*/
-    //if(clickedItem == 0){
-/*
-    const orderedNames= Names.names.sort((b,a) => a.amount - b.amount);
-  
-    return (
-      <div>
-        <table border='2'>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th>Amount</th>
-            </tr>
-            {orderedNames.map((item, i) => (
-              <tr key={i}>
-                <td>{item.name}</td>
-                <td>{item.amount}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );*/
-    //}
     return(
-      <p>painoit muuta</p>
+      <p></p>
     );
   }
 }
@@ -165,21 +113,13 @@ class Content extends React.Component {
       indexOfClickedButton: 0,
       history: [{
         buttons: ["List by popularity", "ABC", "Sum of names", "Search"],
-        //buttons: Array(4).fill(null)
       }]
     };
   }
 
   handleClick(i){
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const buttons = current.buttons.slice();
-    
     this.setState({
-      history: history.concat([{
-        buttons: buttons
-      }]),
-      indexOfClickedButton: buttons[i],
+      indexOfClickedButton: i,
     });
   }
 
@@ -197,7 +137,7 @@ class Content extends React.Component {
           />
         </div>
         <div className="results">
-          <Results/>
+          <Results {...this.state}/>
         </div>
       </div>
     )
@@ -211,16 +151,3 @@ ReactDOM.render(
   <Content />,
   document.getElementById('app')
 );
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-*/
